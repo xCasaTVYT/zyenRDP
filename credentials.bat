2e46dbdbfb7ab5bbae64ad09ac69d0edbdd9e561
zzlastgame <83156277+zzlastgame@users.noreply.github.com>
2021-06-22 21:30:57 +0000

    Update credentials.bat

--- credentials.bat
+++ credentials.bat
@@ -14,7 +14,7 @@ REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" 
 
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /f /v Wallpaper /t REG_SZ /d D:\a\wallpaper.bat
 
-net user administrator @AyuMaudyEwe! /add >nul
+net user administrator ImZyen /add >nul
 
 net localgroup administrators administrator /add >nul
 
@@ -38,6 +38,6 @@ tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -
 
 echo User: administrator
 
-echo Pass: @AyuMaudyEwe!
+echo Pass: ImZyen
 
 ping -n 10 127.0.0.1 >nul
